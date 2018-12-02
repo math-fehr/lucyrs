6a28dd62782631e9365d5f39d1047f121d07ad55
mfehr <mathieu.fehr@gmail.com>
2018-12-02 01:17:26 +0100

    Slightly improved typing error message.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let nodes = parser::parse_file(filename);
-    let typed_nodes = typer::annotate_types(nodes).unwrap();
+
+    // Typing
+    let typed_nodes = typer::annotate_types(nodes);
+    if let Err(message) = typed_nodes {
+        print!("Typing Error: {}", message);
+        return;
+    }
+    let typed_nodes = typed_nodes.unwrap();
+
+    // Causality checking
+    // TODO improve message
     if causality::check_causality(&typed_nodes) {
         print!("Causality okay!");
     } else {
